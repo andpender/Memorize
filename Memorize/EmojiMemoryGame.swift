c697443a73a2e141e7f01ef2b2d130508d1c32c9
Andrew Pender <and.pender@gmail.com>
2020-05-20 20:15:40 +1000

    Initial app ready for assignment

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Andrew Pender on 20/5/20.
+//  Copyright © 2020 Andrew Pender. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","👽","🐮"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
